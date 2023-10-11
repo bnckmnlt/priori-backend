f923058f74780e25b55abde9bd27e19cd7bf7a3a
pigeonpoppy <bryanmontealtomain@gmail.com>
2023-10-11 20:34:57 +0800

    October 11, 2023

--- config/allowed-origins.js
+++ config/allowed-origins.js
@@ -1,5 +1,8 @@
 const allowedOrigins = [
   "https://priorisystems.com",
+  "https://priori.com.ph",
+  "https://priori-website-5ufqk111s-bnckmnlt.vercel.app/",
+  "https://priori-backend.onrender.com",
   "http://localhost:5173",
   "http://localhost:4000",
   "https://dev-z01wb0p0x8tah6u2.us.auth0.com",
